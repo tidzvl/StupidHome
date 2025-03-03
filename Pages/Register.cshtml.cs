83084ef06019b262a61782b64273cff786e8a92c
TiDz <nguyentinvs123@Gmail.com>
2025-03-03 22:47:26 +0700

    done landing page

---
+++ Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetCoreStarter.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
