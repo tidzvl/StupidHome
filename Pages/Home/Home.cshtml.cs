2efef22c4f32262f83c7b413a06cacea36b6ee71
TiDz <nguyentinvs123@Gmail.com>
2025-03-04 14:11:07 +0700

    done menu

---
+++ Pages/Home/Home.cshtml.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StupidHome.Pages.Home
+{
+  public class HomeModel : PageModel
+  {
+    public void OnGet() { }
+  }
+}
