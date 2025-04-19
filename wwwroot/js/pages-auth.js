bbfa4e1b9dd6942b2c90836434d31f3f47641385
TiDz <nguyentinvs123@Gmail.com>
2025-04-19 11:22:17 +0700

    done reg

--- wwwroot/js/pages-auth.js
+++ wwwroot/js/pages-auth.js
@@ -4,6 +4,7 @@
 
 'use strict';
 const formAuthentication = document.querySelector('#formAuthentication');
+const API = document.querySelector('#domain').value;
 
 document.addEventListener('DOMContentLoaded', function (e) {
   (function () {
@@ -87,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
           }),
           submitButton: new FormValidation.plugins.SubmitButton(),
 
-          defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+          // defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
           autoFocus: new FormValidation.plugins.AutoFocus()
         },
         init: instance => {
@@ -97,6 +98,89 @@ document.addEventListener('DOMContentLoaded', function (e) {
             }
           });
         }
+      });
+      fv.on('core.form.valid', function () {
+        const formData = new FormData(formAuthentication);
+        // Chuyển đổi dữ liệu từ FormData thành object
+        const data = Object.fromEntries(formData.entries());
+        $('.card-body').block({
+          message:
+            '<div class="d-flex justify-content-center"><p class="me-2 mb-0">...</p> <div class="sk-wave sk-primary m-0"><div class="sk-rect sk-wave-rect"></div> <div class="sk-rect sk-wave-rect"></div> <div class="sk-rect sk-wave-rect"></div> <div class="sk-rect sk-wave-rect"></div> <div class="sk-rect sk-wave-rect"></div></div> </div>',
+          // timeout: 1000,
+          css: {
+            backgroundColor: 'transparent',
+            border: '0'
+          },
+          overlayCSS: {
+            backgroundColor: '#fff',
+            opacity: 0.8
+          }
+        });
+        fetch(API + '/users/register/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: data.email,
+            password: data.password,
+            username: data.username,
+            ssn: String(Date.now())
+          })
+        })
+          .then(response => {
+            if (!response.ok) {
+              $('.card-body').unblock();
+              Swal.fire({
+                title: 'Ôi không!',
+                text: 'Check backend pls',
+                icon: 'error',
+                customClass: {
+                  confirmButton: 'btn btn-primary'
+                },
+                buttonsStyling: false
+              });
+            }
+            return response.json();
+          })
+          .then(result => {
+            $('.card-body').unblock();
+            if (result['errors']) {
+              Swal.fire({
+                title: 'Ôi không!',
+                text: Object.values(result.errors).find(arr => arr.length > 0)?.[0] || 'Không có lỗi',
+                icon: 'error',
+                customClass: {
+                  confirmButton: 'btn btn-primary'
+                },
+                buttonsStyling: false
+              });
+            } else {
+              Swal.fire({
+                title: 'Chúc mừng!',
+                text: 'Tài khoản của bạn đã được tạo!',
+                icon: 'success',
+                customClass: {
+                  confirmButton: 'btn btn-primary'
+                },
+                buttonsStyling: false
+              }).then(() => {
+                window.location.href = '/login';
+              });
+            }
+          })
+          .catch(error => {
+            $('.card-body').unblock();
+            Swal.fire({
+              title: 'Ôi không!',
+              text: Object.values(response.errors).length > 0 ? Object.values(response.errors)[0][0] : null,
+              icon: 'error',
+              customClass: {
+                confirmButton: 'btn btn-primary'
+              },
+              buttonsStyling: false
+            });
+          });
       });
     }
 
